de0ab4b95bd00fb53419a427b8318df8454e2a46
Duong Dang Hoang <Duongdh89@gmail.com>
2017-12-11 16:21:33 +1100

    Welcome Screen no auto layout

---
+++ woosh/woosh/WelcomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeVC.swift
+//  woosh
+//
+//  Created by Duong Dang Hoang on 12/11/17.
+//  Copyright © 2017 Duong. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func getStartedBtnTouched(_ sender: Any) {
+    }
+}
+
