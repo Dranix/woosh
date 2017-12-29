facd121fe583ada319ba724a0331e0b34a559ab4
Duong Dang Hoang <alex@Duongs-iMac.local>
2017-12-29 16:33:58 +1100

    Model and transfer data btw controller

--- woosh/woosh/Controller/LeagueVC.swift
+++ woosh/woosh/Controller/LeagueVC.swift
@@ -9,24 +9,40 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    var player: Player!
+    @IBOutlet weak var nextBtn: BorderButton!
+    @IBOutlet var selectBtnCollection: [BorderButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func leagueButtonTapped(_ sender: Any) {
+        for btn in selectBtnCollection{
+            btn.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        }
+        let selectedBtn = sender as! UIButton
+        selectedBtn.backgroundColor = UIColor(red: 0.138, green: 0.251, blue: 0.23, alpha: 0.8)
+        
+        nextBtn.isEnabled = true
+        nextBtn.backgroundColor = UIColor(red: 0.138, green: 0.251, blue: 0.23, alpha: 0.8)
+        
+        self.selectLeague(league: selectedBtn.titleLabel!.text!)
     }
-    */
-
+    
+    func selectLeague(league: String){
+        player.desiredLeague = league
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+               skillVC.player = self.player
+        }
+    }
 }
